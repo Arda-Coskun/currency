e736a612b4ff9c00e500c6dee44fb3eab3d0cbae
Arda Coşkun <mail@ardacoskun.dev>
2023-03-03 00:37:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const convertBtn = document.getElementById("convert-btn");
+const usdInput = document.getElementById("usd-input");
+const result = document.getElementById("result");
+
+convertBtn.addEventListener("click", () => {
+  const usdAmount = Number(usdInput.value);
+  const exchangeRate = 8.00;
+  const resultAmount = (usdAmount * exchangeRate).toFixed(2);
+  result.textContent = `${usdAmount} USD is equal to ${resultAmount} TRY`;
+});
 
