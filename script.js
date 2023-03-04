6fdb72fd1a17f35e5ccb0632354204e73ce10a6d
Arda Coşkun <mail@ardacoskun.dev>
2023-03-04 16:39:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,22 @@
-const exchangeBtn = document.getElementById("exchange-btn");
-const amountInput = document.getElementById("amount");
-const fromSelect = document.getElementById("from");
-const toSelect = document.getElementById("to");
-const resultDiv = document.getElementById("result");
 
-const apiEndpoint = "https://api.exchangerate-api.com/v4/latest/";
+var btc = document.getElementById("bitcoin");
+var ltc = document.getElementById("litecoin");
+var eth = document.getElementById("ethereum");
+var doge = document.getElementById("dogecoin");
 
-exchangeBtn.addEventListener("click", async () => {
-  try {
-    const amount = amountInput.value;
-    const fromCurrency = fromSelect.value;
-    const toCurrency = toSelect.value;
-    const response = await fetch(`${apiEndpoint}${fromCurrency}`);
-    const data = await response.json();
-    const exchangeRate = data.rates[toCurrency];
-    const result = amount * exchangeRate;
-    resultDiv.innerHTML = `${amount} ${fromCurrency} = ${result.toFixed(
-      2
-    )} ${toCurrency}`;
-  } catch (error) {
-    console.error(error);
-    resultDiv.innerHTML = "An error occurred while fetching exchange rate data.";
-  }
-});
+var liveprice = {
+    "async": true,
+    "scroosDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
 
-
-
-
-const usdTryRate = document.getElementById("usd-try-rate");
-const eurTryRate = document.getElementById("eur-try-rate");
-const gbpTryRate = document.getElementById("gbp-try-rate");
-
-function getExchangeRates() {
-  fetch("https://api.exchangeratesapi.io/latest?base=TRY&symbols=USD,EUR,GBP")
-    .then(response => response.json())
-    .then(data => {
-      usdTryRate.innerHTML = data.rates.USD.toFixed(2);
-      eurTryRate.innerHTML = data.rates.EUR.toFixed(2);
-      gbpTryRate.innerHTML = data.rates.GBP.toFixed(2);
-    })
-    .catch(error => console.log(error));
+    "method": "GET",
+    "headers": {}
 }
 
-getExchangeRates();
-setInterval(getExchangeRates, 60000); // update every minute
+$.ajax(liveprice).done(function (response){
+    btc.innerHTML = response.bitcoin.usd;
+    ltc.innerHTML = response.litecoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    doge.innerHTML = response.dogecoin.usd;
+
+});
